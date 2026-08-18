b1b880cc7e32ede86bf3a209b9e5180e7ef11ac5
Joao Ferreira <jmiguelff@gmail.com>
2026-08-18 18:19:24 +0100

    fix: use the astro picture feature took increase loading speed, workcover and worknovideo updated

---
+++ src/data/work-images.ts
@@ -0,0 +1,121 @@
+import motorwhipsCover from "../assets/work/2023/motorwhips/motorwhipscover.jpg";
+import motorwhips1 from "../assets/work/2023/motorwhips/motorwhips1.jpg";
+import motorwhips2 from "../assets/work/2023/motorwhips/motorwhips2.jpg";
+import motorwhips3 from "../assets/work/2023/motorwhips/motorwhips3.jpg";
+
+import motorshellCover from "../assets/work/2022/motorshell/motorshellcover.jpg";
+import motorshell1 from "../assets/work/2022/motorshell/motorshell1.jpg";
+import motorshell2 from "../assets/work/2022/motorshell/motorshell2.jpg";
+import motorshell3 from "../assets/work/2022/motorshell/motorshell3.jpg";
+
+import diodoCover from "../assets/work/2020/diodo/diodocover.jpg";
+import diodo1 from "../assets/work/2020/diodo/diodoimage1.jpg";
+import diodo2 from "../assets/work/2020/diodo/diodoimage2.jpg";
+import diodo3 from "../assets/work/2020/diodo/diodoimage3.jpg";
+
+import duneCover from "../assets/work/2020/dune/dunecover.jpg";
+import dune1 from "../assets/work/2020/dune/duneimage1.jpg";
+import dune2 from "../assets/work/2020/dune/duneimage2.jpg";
+import dune3 from "../assets/work/2020/dune/duneimage3.jpg";
+
+import duoCover from "../assets/work/2020/duo/duocover.jpg";
+import duo1 from "../assets/work/2020/duo/duoimage1.jpg";
+import duo2 from "../assets/work/2020/duo/duoimage2.jpg";
+import duo3 from "../assets/work/2020/duo/duoimage3.jpg";
+
+import movingclothCover from "../assets/work/2020/movingcloth/movingclothcover.jpg";
+import movingcloth1 from "../assets/work/2020/movingcloth/movingclothimage1.jpg";
+import movingcloth2 from "../assets/work/2020/movingcloth/movingclothimage2.jpg";
+import movingcloth3 from "../assets/work/2020/movingcloth/movingclothimage3.jpg";
+
+import presencaluminosaCover from "../assets/work/2020/presencaluminosamutavel/presencaluminosacover.jpg";
+import presencaluminosa1 from "../assets/work/2020/presencaluminosamutavel/presencamutavelimage1.jpg";
+import presencaluminosa2 from "../assets/work/2020/presencaluminosamutavel/presencamutavelimage2.jpg";
+import presencaluminosa3 from "../assets/work/2020/presencaluminosamutavel/presencamutavelimage3.jpg";
+import presencaluminosa4 from "../assets/work/2020/presencaluminosamutavel/presencamutavelimage4.jpg";
+
+import presencamutavelCover from "../assets/work/2020/presencamutavel/presencamutavelcover.jpg";
+import presencamutavel1 from "../assets/work/2020/presencamutavel/presencamutavel1.jpg";
+import presencamutavel2 from "../assets/work/2020/presencamutavel/presencamutavel2.jpg";
+
+import videoinstalacaoCover from "../assets/work/2020/videoinstalacao/videoinstalacaocover.jpg";
+import videoinstalacao1 from "../assets/work/2020/videoinstalacao/videoinstalacao1.jpg";
+import videoinstalacao2 from "../assets/work/2020/videoinstalacao/videoinstalacao2.jpg";
+
+import mechanicdrawingCover from "../assets/work/2019/mechanicdrawing/mechanicdrawingcover.jpg";
+import mechanicdrawing1 from "../assets/work/2019/mechanicdrawing/mechanicdrawingimage1.jpg";
+import mechanicdrawing2 from "../assets/work/2019/mechanicdrawing/mechanicdrawingimage2.jpg";
+import mechanicdrawing3 from "../assets/work/2019/mechanicdrawing/mechanicdrawingimage3.jpg";
+
+import organismovivoCover from "../assets/work/2019/organismovivo/organismovivocover.jpg";
+import organismovivo1 from "../assets/work/2019/organismovivo/organismovivoimage1.jpg";
+import organismovivo2 from "../assets/work/2019/organismovivo/organismovivoimage2.jpg";
+import organismovivo3 from "../assets/work/2019/organismovivo/organismovivoimage3.jpg";
+
+import waveCover from "../assets/work/2019/wave/wavecover.jpg";
+import wave1 from "../assets/work/2019/wave/waveimage1.jpg";
+import wave2 from "../assets/work/2019/wave/waveimage2.jpg";
+import wave3 from "../assets/work/2019/wave/waveimage3.jpg";
+
+import soundmachineCover from "../assets/work/2018/soundmachine/soundmachinecover.jpg";
+import soundmachine1 from "../assets/work/2018/soundmachine/soundmachine1.jpg";
+import soundmachine2 from "../assets/work/2018/soundmachine/soundmachine2.jpg";
+import soundmachine3 from "../assets/work/2018/soundmachine/soundmachine3.jpg";
+
+
+export const workImages = {
+    motorwhips: {
+        cover: motorwhipsCover,
+        gallery: [motorwhips1, motorwhips2, motorwhips3]
+    },
+    motorshell: {
+        cover: motorshellCover,
+        gallery: [motorshell1, motorshell2, motorshell3]
+    },
+    diodo: {
+        cover: diodoCover,
+        gallery: [diodo1, diodo2, diodo3]
+    },
+    dune: {
+        cover: duneCover,
+        gallery: [dune1, dune2, dune3]
+    },
+    duo: {
+        cover: duoCover,
+        gallery: [duo1, duo2, duo3]
+    },
+    movingcloth: {
+        cover: movingclothCover,
+        gallery: [movingcloth1, movingcloth2, movingcloth3]
+    },
+    presencaluminosa: {
+        cover: presencaluminosaCover,
+        gallery: [presencaluminosa1, presencaluminosa2, presencaluminosa3, presencaluminosa4]
+    },
+    presencamutavel: {
+        cover: presencamutavelCover,
+        gallery: [presencamutavel1, presencamutavel2]
+    },
+    videoinstalacao: {
+        cover: videoinstalacaoCover,
+        gallery: [videoinstalacao1, videoinstalacao2]
+    },
+    mechanicdrawing: {
+        cover: mechanicdrawingCover,
+        gallery: [mechanicdrawing1, mechanicdrawing2, mechanicdrawing3]
+    },
+    organismovivo: {
+        cover: organismovivoCover,
+        gallery: [organismovivo1, organismovivo2, organismovivo3]
+    },
+    wave: {
+        cover: waveCover,
+        gallery: [wave1, wave2, wave3]
+    },
+    soundmachine: {
+        cover: soundmachineCover,
+        gallery: [soundmachine1, soundmachine2, soundmachine3]
+    }
+}
+
+export default workImages;
